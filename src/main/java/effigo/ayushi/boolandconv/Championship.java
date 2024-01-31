d8e8ae5b0670159fb00f2865bdbace28769a21b4
Vixen-1 <ayushisaxena24111999@gmail.com>
2024-01-31 18:29:28 +0530

    Added boolean and conditionals

---
+++ src/main/java/effigo/ayushi/boolandconv/Championship.java
@@ -0,0 +1,26 @@
+package effigo.ayushi.boolandconv;
+
+public class Championship {
+    public static void main(String[] args) {
+
+        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+
+        int gryffindor = 400;    //gryffindor points
+        int ravenclaw = 200;    //ravenclaw points
+
+        // int margin = amount of points by which gryffindor scored over ravenclaw;
+        int margin = gryffindor - ravenclaw;
+        if (margin >= 300){
+            System.out.println("Gryffindor won! \n");
+        }
+        else if (margin >= 0){
+            System.out.println("Gryffindor is second! \n");
+        }
+        else if(margin >= -100){
+            System.out.println("gryffindor is third! \n");
+        }
+        else{
+            System.out.println("Gryffindor is fourth1");
+        }
+    }
+}
---
+++ src/main/java/effigo/ayushi/boolandconv/WeatherNetwork.java
@@ -0,0 +1,21 @@
+package effigo.ayushi.boolandconv;
+
+
+public class WeatherNetwork {
+    public static void main(String[] args) {        
+        int temp = 25;  
+
+        String forecast;
+        if (temp >= 25){
+            forecast = "its warm! go outside!";
+        }
+        else if(temp >= 10){
+            forecast = "its chilly! wear a coat.";
+        }
+        else{
+            forecast = "its freezing! dont go outside!";
+        }
+        
+        System.out.println(forecast);
+    }
+}
